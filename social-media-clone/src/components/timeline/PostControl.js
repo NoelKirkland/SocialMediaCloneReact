08f6e1d114987686bb846b49e3861953b9334d31
Ian Scott <chesnekov@gmail.com>
2020-09-08 14:12:28 -0700

    update time line in an attempt to dynamically add new posts. incomplete
    NoelKirkland <noelkirkland@gmail.com>
    Co-authored-by: fetonecontrol <frederick.ernest@gmail.com>

---
+++ social-media-clone/src/components/timeline/PostControl.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import TimeLine from './timeline/TimeLine';
+
+class PostControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+
+  render(){
+      <React.Fragment>
+        <TimeLine />
+      </React.Fragment>
+  };
+
+}
+export default PostControl;
--- social-media-clone/src/components/timeline/PostForm.js
+++ social-media-clone/src/components/timeline/PostForm.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import { v4 } from 'uuid';
 
-function PostForm(){
+function PostForm(props){
+
+  function addNewPost(event){
+    event.preventDefault();
+    console.log(event.target.author.value);
+    console.log(event.target.body.value);
+    // props.onNewTicketCreation({author: event.target.author.value, body: event.target.body.value, id: v4()});
+  }
+
+
   return(
-    <form className="post-form" action="/action_page.php">
+    <React.Fragment>
+    {/* <form className="post-form" action="/action_page.php">
       <div className="form-group">
         <input type="text" className="form-control" placeholder="What is on your mind?" />
       </div>
       <button type="submit" className="btn btn-default">Post!</button>
+    </form> */}
+
+    <form onSubmit={addNewPost}>
+    <input
+      type='text'
+      author='author'
+      placeholder='who are you?' />
+    <input 
+      type='text'
+      body='body'
+      placeholder='What is on your mind today?' />
+    <button type='submit'>Post!</button>
     </form>
+    </React.Fragment>
   );
 }
 
